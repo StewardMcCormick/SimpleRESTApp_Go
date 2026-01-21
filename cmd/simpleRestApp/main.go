6a10f1bdfc59d9d4bd1a8b5a3916e188dda2817d
Egor Bessonov <bessonoven@mail.ru>
2026-01-21 23:32:18 +0300

    Create base HTTP Server

--- cmd/simpleRestApp/main.go
+++ cmd/simpleRestApp/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "net/http"
 
+const (
+	host = "localhost"
+	port = "8080"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    host + ":" + port,
+		Handler: mux,
+	}
+
+	_ = server.ListenAndServe()
 }
