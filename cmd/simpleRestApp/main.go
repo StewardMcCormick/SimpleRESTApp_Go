523754a4403ce64f7a67b864f6747b268aa22eac
Egor Bessonov <bessonoven@mail.ru>
2026-01-21 23:36:27 +0300

    Add /hello endpoint

--- cmd/simpleRestApp/main.go
+++ cmd/simpleRestApp/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 const (
 	host = "localhost"
@@ -10,10 +13,16 @@ const (
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("GET /api/hello", hello)
+
 	server := &http.Server{
 		Addr:    host + ":" + port,
 		Handler: mux,
 	}
 
 	_ = server.ListenAndServe()
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello")
 }
